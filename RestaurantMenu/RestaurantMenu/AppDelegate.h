a13d47b9bd9b1ce023e36ebbb27afc2212f1ebb0
Jaime Garcia <jaimiho@gmail.com>
2016-04-20 16:47:25 -0400

    Restaurant Menu

---
+++ RestaurantMenu/RestaurantMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RestaurantMenu
+//
+//  Created by Jonathan Pacheco on 30/07/15.
+//  Copyright (c) 2015 Grability. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
